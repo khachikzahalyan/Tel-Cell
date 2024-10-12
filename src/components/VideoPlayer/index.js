445091cd468c16e9c7848eb86ffd4eab8c06abb0
khachikzahalyan <zahalyanxcho@gmail.com>
2024-10-12 11:55:51 +0400

    Change YouTube URL

--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const VideoPlayer = () => {
-  const videoId = "rzRqEWJYwX4";
+  const videoId = "An14pbQ8IRg"; // ID вашего видео
 
   return (
     <div
@@ -11,18 +11,33 @@ const VideoPlayer = () => {
         left: 0,
         width: "100%",
         height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "black", // Чтобы черный фон был за видео
         overflow: "hidden"
       }}
     >
-      <iframe
-        width="100%"
-        height="100%"
-        src={`https://www.youtube.com/embed/${videoId}?autoplay=1&controls=0&mute=1&showinfo=0`}
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+      <div
+        style={{
+          width: "100%",
+          height: "100%"
+        }}
+      >
+        <iframe
+          width="100%"
+          height="100%"
+          src={`https://www.youtube.com/embed/${videoId}?autoplay=1&controls=0&mute=1`}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          style={{
+            width: "100%",
+            height: "100%"
+          }}
+        ></iframe>
+      </div>
     </div>
   );
 };
