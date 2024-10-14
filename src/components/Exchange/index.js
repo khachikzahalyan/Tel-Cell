7b3f43642ae6ee12b02b31c9748a9f5f38c71da6
Davit Sargsyan <davit_sargsyan@Davits-MacBook-Pro.local>
2024-10-14 11:53:37 +0400

    commit

--- src/components/Exchange/index.js
+++ src/components/Exchange/index.js
@@ -8,6 +8,7 @@ import "./styles.css";
 
 const Exchange = () => {
   const [rates, setRates] = useState({ buy: "", sell: "" });
+  console.log("rates: ", rates);
 
   const formattedDate = format(new Date(), "MMMM dd, yyyy, HH:mm", {
     locale: hy,
