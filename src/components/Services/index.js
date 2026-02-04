66763da8af331664118ed53b1e2e8ef1864a5750
khachik.zahalyan <khachik.zahalyan@nurselink.io>
2026-02-04 20:06:11 +0400

    Add styles, delete old texts

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -11,23 +11,9 @@ function Services() {
       </div>
       <div className="generalBox">
         <div className="box1">
-          <h2>Բջջային օպերատորներ</h2>
-          <div className="operators">
-            <div className="company">
-              <p>Viva MTS</p>
-              <p>Team Telecom</p>
-            </div>
-            <div className="operatorsPrecent">
-              <span className="zero">0</span>
-              <span className="percent">%</span>
-              <br></br>
-              <span className="x">միջնորդավճար</span>
-            </div>
-          </div>
           <h2>Ինտերնետ և IP հեռախոսակապ</h2>
           <div className="operators">
-            <div className="company">
-              <p>Ucom</p>
+            <div className="company-center">
               <p>Rostelecom</p>
             </div>
             <div className="operatorsPrecent">
@@ -39,14 +25,6 @@ function Services() {
           </div>
           <h2>Բանկեր</h2>
           <div className="operatorsBank">
-            {/* <div className="banksBox">
-              <p>ՎՏԲ - Հայաստան Բանկ ՓԲԸ </p>
-              <p>300֏</p>
-            </div> */}
-            {/* <div className="banksBox">
-              <p>Ինեկոբանկ ԹԲԸ</p>
-              <p>400֏</p>
-            </div> */}
             <div className="banksBox">
               <div
                 style={{
@@ -66,10 +44,7 @@ function Services() {
               </div>
               <p>300֏</p>
             </div>
-            {/* <div className="banksBox">
-              <p>Արդշինբանկ ՓԲԸ</p>
-              <p>500֏</p>
-            </div> */}
+
           </div>
         </div>
         <div className="box2">
